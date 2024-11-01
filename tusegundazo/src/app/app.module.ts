64770d9a9a118e1bd17fd6b424a5c4c9b404b654
jposoriob1 <jp.osoriob1@uniandes.edu.co>
2024-11-01 17:23:35 -0500

    Diseño y maquetacion vehiculos landing

--- tusegundazo/src/app/app.module.ts
+++ tusegundazo/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,10 +15,11 @@ import { VehiculoModule } from './vehiculo/vehiculo.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    VehiculoModule
+    VehiculoModule,
+
   ],
   providers: [
-    provideClientHydration()
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
--- tusegundazo/src/app/vehiculo/vehiculo.component.ts
+++ tusegundazo/src/app/vehiculo/vehiculo.component.ts
@@ -11,11 +11,28 @@ export class VehiculoComponent implements OnInit {
 
   constructor(private vehiculoService: VehiculoService) { }
   vehiculos: Array<Vehiculo> = [];
+  consolidado: Array<any> = [];
+  vehiculosAgrupados: { marca: string, cantidad: number }[] = [];
 
   getVehiculos(){
     this.vehiculoService.getVehiculos().subscribe(vehiculos => {
       this.vehiculos = vehiculos;
+      this.vehiculosAgrupados = this.agruparPorMarca(this.vehiculos);
     })
+  }
+
+  agruparPorMarca(vehiculos: Vehiculo[]): { marca: string, cantidad: number }[] {
+    return vehiculos.reduce((acc: { marca: string, cantidad: number }[], vehiculo) => {
+      const marcaExistente = acc.find(item => item.marca === vehiculo.marca);
+
+      if (marcaExistente) {
+        marcaExistente.cantidad += 1;
+      } else {
+        acc.push({ marca: vehiculo.marca, cantidad: 1 });
+      }
+
+      return acc;
+    }, []);
   }
 
   ngOnInit() {
